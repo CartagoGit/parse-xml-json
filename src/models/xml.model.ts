fb15ac8fcca49fc55bda7699337546eb0c339919
Cartago <cartago.relaxingcup@gmail.com>
2023-12-24 02:38:57 +0100

    Empezamos a avanzar a separar los tags de los xml

--- src/models/xml.model.ts
+++ src/models/xml.model.ts
@@ -11,7 +11,7 @@ interface XmlProps {
 	version: string;
 	xmlTag: string | null;
 	encoding: string | null;
-	children: string[];
+	children: XmlChild[];
 	parent: XmlFile | XmlChild | null;
 	level: number;
 }
@@ -27,8 +27,7 @@ export class XmlFile implements XmlProps {
 	public readonly version: string;
 	public readonly xmlTag: string | null = null;
 	public readonly encoding;
-	// public readonly children: XmlItem[] = [];
-	public readonly children: string[] = [];
+	public readonly children: XmlChild[] = [];
 	public readonly parent: XmlFile | null = null;
 	public readonly level = 0;
 
@@ -65,42 +64,83 @@ export class XmlFile implements XmlProps {
 		return encoding;
 	}
 
-	private _getChildrenTags(): string[] {
-		const elementPattern = /(<[^\/>]+\/>|<[^>]+>)/g;
-		let matches = this.content.trim().match(elementPattern);
-		if (this.xmlTag) {
-			matches?.shift();
-		}
-		const elements = matches ?? [];
-		let tags: string[] = [];
-		for (let i = 0; i < elements.length; i++) {
-			const element = elements[i];
-			const inner = element.replace(/[<\/\>]/g, '')?.trim() ?? '';
-			const nameTag = inner.split(' ')[0];
-			if (element.includes('/>')) {
-				tags.push(nameTag);
+	private _getChildrenTags(): XmlChild[] {
+		let text = this.content
+			.replace(/[\n\r]/g, '')
+			.replace(/>\s*</g, '><')
+			.trim();
+		if (this.xmlTag) text = text.replace(this.xmlTag, '');
+		const tags: XmlChild[] = [];
+		while (!!text) {
+			const indexEnd = text.indexOf('>');
+			const openTag = text.slice(0, indexEnd + 1);
+			if (openTag.includes('/>')) {
+				tags.push(
+					new XmlChild({
+						tag: openTag.replace(/[<\/\>]/g, '')?.trim() ?? '',
+						content: openTag,
+						parent: this,
+						level: this.level + 1,
+					})
+				);
+				text = text.slice(indexEnd + 1);
 			} else {
-				const searchTag = nameTag;
-				let counter = 1;
-				for (let j = i + 1; j < elements.length; j++) {
-					const elementInner = elements[j];
-					if (elementInner.includes(searchTag)) {
-						if (elementInner.includes('/')) {
-							counter--;
-							if (counter === 0) {
-								tags.push(nameTag);
-								i = j;
-								break;
-							} else counter++;
-						}
+				const nameTag = openTag.replace(/[<\/\>]/g, '')?.trim() ?? '';
+				let textSearching = text.slice(indexEnd + 1);
+
+				while (true) {
+					const openSameTag = textSearching.indexOf('<' + nameTag);
+					const closeSameTag = textSearching.indexOf('</' + nameTag);
+					if (openSameTag !== -1 && openSameTag < closeSameTag) {
+						textSearching = textSearching.slice(closeSameTag + 1);
+					} else {
+						const textTag = textSearching.slice(0, closeSameTag);
+						const xmlChild = new XmlChild({
+							tag: nameTag,
+							content: textTag,
+							parent: this,
+							level: this.level + 1,
+						});
+						tags.push(xmlChild);
+						textSearching = textSearching.slice(closeSameTag + 1);
+						const lastTagIndex = textSearching.indexOf('>');
+						text = textSearching.slice(lastTagIndex + 1);
+						break;
 					}
 				}
 			}
 		}
+
 		return tags;
 	}
 }
 
 class XmlChild {
-	
+	public readonly name: string;
+	public readonly content: string;
+	public readonly children: XmlChild[] = [];
+	public readonly parent: XmlFile | XmlChild | null = null;
+	public readonly level;
+	public readonly openTag: string;
+	public readonly closeTag: string | null = null;
+	public readonly isSelfClosing: boolean;
+
+	constructor(props: {
+		tag: string;
+		content: string;
+		parent: XmlFile | XmlChild;
+		level: number;
+	}) {
+		const { tag, content, parent, level } = props;
+		this.name = tag.split(' ')[0];
+		this.isSelfClosing = content.endsWith('/>');
+
+		// this.openTag = this.isSelfClosing ? content : `<${tag}>`;
+		this.openTag = 'ee'
+		// this.closeTag = this.isSelfClosing ? null : `</${this.name}>`;
+		this.closeTag = 'asdsad'
+		this.parent = parent;
+		this.content = content;
+		this.level = level;
+	}
 }
